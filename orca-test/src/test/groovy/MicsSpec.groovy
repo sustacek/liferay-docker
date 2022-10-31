282fd0da89bedd2157d7ae3e0a9154cba8f604bb
Josef Sustacek <josef.sustacek@liferay.com>
2022-10-31 13:48:55 +0100

    Cover more features of the "build" command

--- orca-test/src/test/groovy/MicsSpec.groovy
+++ orca-test/src/test/groovy/MicsSpec.groovy
@@ -7,7 +7,7 @@ class MicsSpec extends BaseOrcaSpec {
 
     def "'' (no command) should print a help message and return error"() {
         when:
-            def orcaRun = orca.startOrca('')
+            def orcaRun = orca.startOrca([:], '')
         
             orcaRun.waitFor(15, TimeUnit.SECONDS)
 
